468fb248b67f82469c82d55f11f78078aa076552
Julijero <julianjeronimon@gmail.com>
2018-04-11 17:31:11 -0500

    second modified avianca.sql

--- avianca.sql
+++ avianca.sql
@@ -19,10 +19,10 @@ grant DBA, connect to AVIANCA
 i. If you are using Oracle 11g: Create sequences for every primary key.
 ii. If you are using Oracle 12c: Use identity columns.
 b. Create primary and foreign keys.*/
-create table Tripulacion
+create table Tripulaciones
 (
-    id_empleados number(20) not null primary key,
-    id_azafatas number(20),
+    id_empleados integer not null primary key,
+    id_azafatas integer,
     nombres varchar2(255),
     apellidos varchar2(255),
     fecha_de_nacimiento date,
@@ -34,7 +34,7 @@ create table Tripulacion
     estado varchar(255) not null check(estado in ('en vuelo','activo','inactivo','jubilado','suspendido','despedido','entrenamiento','licencia','vacaciones')),
     ubicacion_actual varchar(255)
 );
-
+ drop table tripulacion
 create table Pilotos
 (
     id_piloto number(20) not null primary key,
@@ -42,7 +42,7 @@ create table Pilotos
     cantidad_horas_vuelo integer,
     tipo_licencia varchar(255) not null check(tipo_licencia in ('CPL','IFR','ME','ATPL')),
     cargo varchar(255) not null check(cargo in ('comandante','primer oficial')),
-    id_empleado number(20)  
+    id_empleado integer  
 );
 
 create table Aeropuertos
@@ -64,12 +64,12 @@ create table Rutas
     distancia_kilometro integer,
     numero_vuelo integer,
     frecuencia_semanal integer,
-    aeronave_requerida varchar2(255),
+    aeronave_requerida varchar2(45),
     cantidad_promedio integer,
     tripulante_requerido integer
 );
 
-create table log_vuelo
+create table logs_vuelo
 (
     id_log_vuelo integer not null primary key,
     log_timestamp timestamp,
@@ -80,24 +80,20 @@ create table log_vuelo
     direccion varchar(255),
     id_vuelo integer not null  
 );
- 
+ drop table log_vuelo
 
-create table Vuelo
+create table Vuelos
 (
     id_vuelo integer not null primary key,
-    id_piloto number(20) not null,
-    id_copiloto number(20) not null,
-    id_auxiliares number(20) not null,
-    hora_estimada_salida varchar(255),
-    hora_estimada_llegada varchar(255),
-    hora_real_salida varchar(255),
-    hora_real_llegada varchar(255),
+    id_piloto integer not null,
+    id_copiloto integer not null,
+    id_auxiliares integer not null,
     duracion_real integer,
     aeronave_asignada varchar(255),
     cantidad_pasajeros integer,
     id_avion integer not null
 );
-
+ drop table vuelo
 create table checkin
 (
     id_checkin integer not null primary key,
@@ -111,18 +107,21 @@ create table checkin
 );
 
 
-create table programacion
+create table itinerarios
 (
-    id_programacion integer not null primary key,
+    id_itinerario integer not null primary key,
     id_ruta integer not null,
     id_vuelo integer not null,
     id_avion integer not null,
-    hora varchar2(255),
+    hora_estimada_salida varchar(255),
+    hora_estimada_llegada varchar(255),
+    hora_real_salida varchar(255),
+    hora_real_llegada varchar(255),
     pais varchar2(255),
     id_aeropuerto integer not null
 );
 
-create table avion 
+create table aviones 
 (
     id_avion integer not null primary key,
     registration varchar(50),
@@ -131,13 +130,12 @@ create table avion
     aircraft_type varchar(255),
     bussiness_seats integer,
     economy_seats integer
-    
 );
 
 ALTER TABLE Pilotos
 ADD CONSTRAINT FK_Pilotos_Tripulacion
   FOREIGN KEY (id_empleado)
-  REFERENCES Tripulacion(id_empleados);
+  REFERENCES Tripulaciones(id_empleados);
   
   ALTER TABLE Rutas
 ADD CONSTRAINT FK_ruta_id_aeropuerto_origen
@@ -149,53 +147,50 @@ ADD CONSTRAINT FK_ruta_id_aeropuerto_destino
   FOREIGN KEY (id_aeropuerto_destino)
   REFERENCES Aeropuertos(id_aeropuerto);
   
-ALTER TABLE log_vuelo
+ALTER TABLE logs_vuelo
 ADD CONSTRAINT FK_log_vuelo_id_vuelo
   FOREIGN KEY (id_vuelo)
-  REFERENCES Vuelo(id_vuelo);
+  REFERENCES Vuelos(id_vuelo);
 
-ALTER TABLE Vuelo
+ALTER TABLE Vuelos
 ADD CONSTRAINT FK_vuelo_id_piloto
   FOREIGN KEY (id_piloto)
   REFERENCES Pilotos(id_piloto);
   
-  ALTER TABLE Vuelo
+  ALTER TABLE Vuelos
 ADD CONSTRAINT FK_vuelo_id_copiloto
   FOREIGN KEY (id_copiloto)
   REFERENCES Pilotos(id_piloto);
   
-  ALTER TABLE Vuelo
-ADD CONSTRAINT FK_vuelo_id_copiloto
-  FOREIGN KEY (id_copiloto)
-  REFERENCES Pilotos(id_piloto);
+
   
-  ALTER TABLE Vuelo
+  ALTER TABLE Vuelos
 ADD CONSTRAINT FK_vuelo_id_avion
   FOREIGN KEY (id_avion)
-  REFERENCES avion(id_avion);
+  REFERENCES aviones(id_avion);
   
     ALTER TABLE checkin
 ADD CONSTRAINT FK_checkin_id_vuelo
   FOREIGN KEY (numero_vuelo)
-  REFERENCES Vuelo(id_vuelo);
+  REFERENCES Vuelos(id_vuelo);
   
   
-  ALTER TABLE programacion
-ADD CONSTRAINT FK_programacion_id_ruta
+  ALTER TABLE itinerarios
+ADD CONSTRAINT FK_itinerarios_id_ruta
   FOREIGN KEY (id_ruta)
   REFERENCES Rutas(id_ruta);
   
-  ALTER TABLE programacion
+  ALTER TABLE itinerarios
 ADD CONSTRAINT FK_programacion_id_vuelo
   FOREIGN KEY (id_vuelo)
-  REFERENCES Vuelo(id_vuelo);
+  REFERENCES Vuelos(id_vuelo);
   
-   ALTER TABLE programacion
+   ALTER TABLE itinerarios
 ADD CONSTRAINT FK_programacion_id_avion
   FOREIGN KEY (id_avion)
-  REFERENCES avion(id_avion);
+  REFERENCES aviones(id_avion);
   
-  ALTER TABLE programacion
+  ALTER TABLE itinerarios
 ADD CONSTRAINT FK_programacion_id_aeropuerto
   FOREIGN KEY (id_aeropuerto)
   REFERENCES Aeropuertos(id_aeropuerto);
